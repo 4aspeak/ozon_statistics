bb3bee6cc4b5cb63d136dab0e07c59558933f276
sbr <sungatullinbulat05@gmail.com>
2025-01-11 21:29:01 +0300

    update

--- app/my_celery.py
+++ app/my_celery.py
@@ -12,8 +12,8 @@ app = Celery('app', broker='redis://redis:6379/0')
 app.conf.timezone = 'Europe/Moscow'
 
 
-@app.task(name='main_task')
-def main_task():
+@app.task(name='at_last_day_task')
+def at_last_day_task():
     date = datetime.datetime.now()
     main_date = datetime.datetime(
         year=date.year, month=date.month, day=date.day-1, hour=0, minute=0, second=0, tzinfo=pytz.timezone('Europe/Moscow')
@@ -22,8 +22,8 @@ def main_task():
     main(main_date)
 
 
-@app.task(name='every_3_hours_task')
-def every_3_hours_task():
+@app.task(name='at_this_day_task')
+def at_this_day_task():
     date = datetime.datetime.now()
     main_date = datetime.datetime(
         year=date.year, month=date.month, day=date.day, hour=0, minute=0, second=0,
@@ -35,11 +35,35 @@ def every_3_hours_task():
 
 app.conf.beat_schedule = {
     'run-main-task-every-day-at-1am': {
-        'task': 'main_task',
+        'task': 'at_last_day_task',
         'schedule': crontab(hour=0, minute=30),
     },
-    'run-main-task-every-3-hours-1': {
-        'task': 'every_3_hours_task',
-        'schedule': crontab(hour='*/3'),
+    'run-main-task-every-day-at-3am': {
+        'task': 'at_this_day_task',
+        'schedule': crontab(hour=3, minute=0),
+    },
+    'run-main-task-every-day-at-6am': {
+        'task': 'at_this_day_task',
+        'schedule': crontab(hour=6, minute=0),
+    },
+    'run-main-task-every-day-at-9am': {
+        'task': 'at_this_day_task',
+        'schedule': crontab(hour=9, minute=0),
+    },
+    'run-main-task-every-day-at-15pm': {
+        'task': 'at_this_day_task',
+        'schedule': crontab(hour=15, minute=0),
+    },
+    'run-main-task-every-day-at-18pm': {
+        'task': 'at_this_day_task',
+        'schedule': crontab(hour=18, minute=0),
+    },
+    'run-main-task-every-day-at-21pm': {
+        'task': 'at_this_day_task',
+        'schedule': crontab(hour=21, minute=0),
+    },
+    'run-main-task-every-day-at-21-35pm': {
+        'task': 'at_this_day_task',
+        'schedule': crontab(hour=21, minute=35),
     },
 }
